9d4cb6905ace0f6f522e4372dce74be69497235a
bbhavanasree2212-code <bbhavanasree2212@gmail.com>
2026-09-17 15:42:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 with open("exam_report.txt", "w") as f:
     f.write("ONLINE EXAMINATION REPORT\n")
     f.write("=========================\n")
-    f.write("Student: Archana\n")
+    f.write("Student: BHAVANASREE\n")
     f.write("Total Marks: 100\n")
-    f.write("Marks Obtained: 82\n")
+    f.write("Marks Obtained: 90\n")
     f.write("Result: PASS\n")
 
 print("Exam report generated successfully.")
